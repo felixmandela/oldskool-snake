e63a1da20796f8f2e560028a4bf6c7b3fa77cc38
Felix Mandela <felixmandela95@gmail.com>
2023-05-03 21:36:09 +0700

    add snake

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const playarea = document.querySelector(".play-area");
+document.addEventListener("keydown", changeDirection)
 
-let foodX = Math.floor(Math.random() * 30) + 1;
-let foodY = Math.floor(Math.random() * 30) + 1;
+let foodX;
+let foodY;
+let snakeX = 15;
+let snakeY = 15;
+let newPosition = '';
+let moveX = 0;
+let moveY = 0;
 
-const runGame = () => {
-    playarea.innerHTML = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`
+const randomFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
 }
 
-runGame()
+function changeDirection(d) {
+    if (d.key == "ArrowUp") {
+        moveX = 0;
+        moveY = -1;
+    }
+    else if (d.key == "ArrowDown") {
+        moveX = 0;
+        moveY = 1;
+    }
+    else if (d.key == "ArrowLeft") {
+        moveX = -1;
+        moveY = 0;
+    }
+    else if (d.key == "ArrowRight") {
+        moveX = 1;
+        moveY = 0;
+    };
+}
+
+const runGame = () => {
+    snakeX += moveX;
+    snakeY += moveY;
+    newPosition = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    newPosition += `<div class="player" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    playarea.innerHTML = newPosition;
+}
+
+
+randomFoodPosition();
+setInterval(runGame, 1000 / 10)
